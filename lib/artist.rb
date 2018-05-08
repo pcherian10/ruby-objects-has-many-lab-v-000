7b465c6b8d171190039dd3c00cfe2bdf1a01f3de
Paul <pcherian10@gmail.com>
2018-05-08 18:06:05 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,12 +9,14 @@ class Artist
 
   def add_song (song)
     @songs << song
+    @@song_count += 1
     song.artist = self
   end
 
   def add_song_by_name (name)
     song = Song.new(name)
     @songs << song
+    @@song_count += 1
     song.artist = self
   end
 
--- lib/author.rb
+++ lib/author.rb
@@ -9,7 +9,7 @@ class Author
 
   def add_post (post)
     @posts << post
-    @@post_count += 1 
+    @@post_count += 1
     post.author = self
   end
 
