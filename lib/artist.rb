4e629c142bca9a6fc539c6e117750157552c09cd
Paul <pcherian10@gmail.com>
2018-05-08 18:03:41 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,12 +15,11 @@ class Artist
   def add_song_by_name (name)
     song = Song.new(name)
     @songs << song
-    @@song_count = @song.length
     song.artist = self
   end
 
   def self.song_count
-    @@song_count
+    @@song_count = @songs.length
   end
 
   def songs
