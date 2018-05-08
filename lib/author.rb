be242c9c825e5cd054f2806fbdb9618a18bbb222
Paul <pcherian10@gmail.com>
2018-05-08 17:55:30 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -11,6 +11,12 @@ class Author
     post.author = self
   end
 
+  def add_post_by_title (title)
+    post = Post.new(title)
+    @posts << post
+    post.author = self
+  end
+
   def posts
     @posts
   end
